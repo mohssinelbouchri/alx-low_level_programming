bd2b821d9af51bcfb0309bcfaa8c2b00e1db40fb
mohssinelbouchri <mohssinelbouchiri@gmail.com>
2023-07-27 05:33:02 -0700

    j

--- 0x06-pointers_arrays_strings/1_strncat.c
+++ 0x06-pointers_arrays_strings/1_strncat.c
@@ -1,18 +1,18 @@
 #include "main.h"
 
 /**
- * _strncat - function that concatenates tow strings.
+ * _strncat - a function that concatenates tow strings.
  *
  * @dest: pointer to destination input
  * @src: pointer to source input
- * @n: most number pf bytes from @src
+ * @n: most number of bytes from @src
  *
  * Return: @dest
 */
 
 char *_strncat(char *dest, char *src, int n)
 {
-        int c, i;
+	int c, i;
 
 	c = 0;
 
@@ -22,7 +22,7 @@ char *_strncat(char *dest, char *src, int n)
 
 	/**
 	 * src does not need to be null terminated
-	 * if it contains n or mor bytes
+	 * if it contains n or more bytes
 	*/
 	for (i = 0; i < n && src[i] != '\0'; i++)
 		dest[c + i] = src[i];
