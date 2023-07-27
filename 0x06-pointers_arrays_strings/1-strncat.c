cd1bb4741594be36f8012e8f7595115dbab97591
mohssinelbouchri <mohssinelbouchiri@gmail.com>
2023-07-27 06:25:45 -0700

    h

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -15,7 +15,7 @@ char *_strncat(char *dest, char *src, int n)
 	int c, i;
 
 	c = 0;
-	
+
 	/*find the size of dest array*/
 	while (dest[c])
 		c++;
@@ -28,6 +28,6 @@ char *_strncat(char *dest, char *src, int n)
 		dest[c + i] = src[i];
 	/*null terminate dest*/
 	dest[c + i] = '\0';
-	
+
 	return (dest);
 }
