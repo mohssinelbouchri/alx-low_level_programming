2630152f840c1c86316f31d912de933a5debaaaf
mohssinelbouchri <mohssinelbouchiri@gmail.com>
2023-07-27 08:08:39 -0700

    j

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -7,7 +7,7 @@
  * Return: 1 if true, 0 if false
 */
 
-int islower(char c)
+int isLower(char c)
 {
 	return (c >= 97 && c <= 122);
 }
@@ -27,4 +27,31 @@ int isDelimiter(char c)
 		if (c == delimiter[i])
 			return (1);
 	return (0);
+}
+
+/**
+ * cap_string - capitalizes all words of string
+ * @s: input string
+ * Return: string with capitalized words
+*/
+
+char *cap_string(char *s)
+{
+	char *ptr = s;
+	int foundDelimit = 1;
+
+	while (*s)
+	{
+		if ( isDelimiter(*s))
+			foundDelimit = 1;
+		else if (isLower(*s) && foundDelimit)
+		{
+			*s -= 32;
+			foundDelimit = 0;
+		}
+		else
+			foundDelimit = 0;
+		s++;
+	}
+	return (ptr);
 }
