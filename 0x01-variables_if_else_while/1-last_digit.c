aa87be3154bfcffa11330902deb893054bf97155
mohssinelbouchri <mohssinelbouchiri@gmail.com>
2023-08-01 08:53:49 -0700

    commit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -20,7 +20,7 @@ int main(void)
 	digit = n % 10;
 
 	if (digit > 5)
-		printf("last digit of %i is %i and greater than 5\n", n, digit);
+		printf("last digit of %i is %i and is greater than 5\n", n, digit);
 	else if (digit == 0)
 		printf("last digit of %i is %i and is 0\n", n, digit);
 	else if (digit < 6 && digit != 0)
