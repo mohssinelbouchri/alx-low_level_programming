250801e4687c90c7de70b254bef4e839a7a963a8
mohssinelbouchri <mohssinelbouchiri@gmail.com>
2023-08-01 11:08:23 -0700

    h

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -11,12 +11,13 @@
 
 int main(void)
 {
-	int digit - 0;
+	int digit = 0;
 
 	while (digit <= 9)
 	{
 		/*convert digit to ASCII representation*/
 		putchar(digit + '0');
+		digit++;
 	}
 	putchar('\n');
 
