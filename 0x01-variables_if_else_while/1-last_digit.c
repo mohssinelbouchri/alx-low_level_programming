857b5d05f41c8f1f2f82a472f5e397846eb15f04
mohssinelbouchri <mohssinelbouchiri@gmail.com>
2023-08-01 09:49:00 -0700

    commit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -17,7 +17,7 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	digit = (n % 10);
+	digit = n % 10;
 
 	if (digit > 5)
 		printf("last digit of %i is %i and is greater than 5\n", n, digit);
